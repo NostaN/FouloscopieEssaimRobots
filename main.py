f5401cbc39804d0f7a96da955f1ffa10f3dbe08c
Nostan <dragyon@msn.com>
2021-06-01 20:26:43 +0200

    premiere version

---
+++ main.py
@@ -0,0 +1,12 @@
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def main():
+
+   #print("Hey You!")
+windows_size_x= 800
+windows_size_z= 600
+
+if __name__ == '__main__':
+    main()
+
